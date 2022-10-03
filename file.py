062aa51ad9adaecf22e25fb4002c7e45d0bb2d77
Clara Florindo Rabadan <claraflorindo@MacBook-Pro-de-Clara-2.local>
2022-10-03 11:03:07 +0200

    rename the function to clara

--- file.py
+++ file.py
@@ -1,2 +1,12 @@
-def sum(a,b):
+def clara(a,b):
     return a+b
+
+
+
+
+
+# para hacer el push a git : git push origin master
+#username : claraflorindo
+#password : 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
+
+#para hacer pull de git a visual : git pull origin master
